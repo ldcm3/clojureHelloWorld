d76bcf33c32588bf638eb72e4b0d2efc00a2a063
Lucas <lucasdacruzmendes@hotmail.com>
2017-11-30 01:11:58 -0200

    first commit

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,24 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(println "Cleanliness is next to godliness")
+
+
+(defn train
+  []
+  (println "Test bem louco"))
+
+(defn test
+  []
+  (println "TESTESTSTSTEST"))
+
+(+ 1 (|* 2 3 4))
+
+
+
+
